06caec2ed0a2236beb808799d224072d941542d3
Jarrod Bogard <jarrodbogard@gmail.com>
2022-05-10 22:22:25 -0500

    passes unit testing

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
@@ -22,23 +23,54 @@ const generateSolution = () =>  {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
   }
+  console.log(solution, "----solution----")
 }
 
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+  let solutionArray = solution.split("")
+  let guessArray = guess.split("")
+  let hintOne = 0
+  let hintTwo = 0
+  guessArray.forEach((element, i) => {
+    if(guessArray[i] === solutionArray[i]) {
+      hintOne++
+      guessArray[i] = 0
+      solutionArray[i] = 1
+      console.log(guessArray[i], "----matches----", solutionArray[i], "-----matches-----")
+    }
+  })
+  guessArray.forEach((element, i) => {
+    solutionArray.forEach((solutionLetter, index) => {
+      if(element === solutionLetter) {
+        hintTwo++
+        guessArray[i] = 0
+        solutionArray[index] = 0
+      }
+    })
+  })
+  console.log(hintOne, "-", hintTwo)
+  return hintOne + "-" + hintTwo
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  if(guess === solution) {
+    // console.log("Winner")
+    return "You guessed it!"
+  } else {
+    board.push(guess)
+    generateHint(guess)
+  }
 }
 
 
 const getPrompt = () =>  {
+  console.log(board, "---board---")
   rl.question('guess: ', (guess) => {
     mastermind(guess);
     printBoard();
